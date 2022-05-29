0889703dfef33364236725c690c1860d24c250ff
Jonnas Freire <jonnas.freire17@gmail.com>
2022-05-29 16:59:46 -0300

    ..

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 
-import { products } from '../products';
 
 @Component({
   selector: 'app-product-list',
@@ -13,10 +12,7 @@ export class ProductListComponent {
   share() {
     window.alert('The product has been shared!');
   }
-
-  onNotify() {
-    window.alert('You will be notified when the product goes on sale');
-  }
+  
 }
 
 
