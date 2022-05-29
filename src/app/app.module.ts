4c929ca9772a16de50f0c3650bf0adbb4fb19290
Jonnas Freire <jonnas.freire17@gmail.com>
2022-05-29 17:01:16 -0300

    ..

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,8 +14,7 @@ import { ProductDetailsComponent } from './product-details/product-details.compo
     BrowserModule,
     ReactiveFormsModule,
     RouterModule.forRoot([
-      { path: '', component: ProductListComponent },
-      { path: 'products/:productId', component: ProductDetailsComponent }
+      { path: '', component: ProductListComponent }
     ])
   ],
   declarations: [
